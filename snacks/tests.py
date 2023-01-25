deedb85cfb90229af3d130f722580ea22f2ae225
Monica <ramirez.92.monica@gmail.com>
2023-01-24 19:36:52 -0600

    add test

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class SnackTest(TestCase):
+
+    def test_list_page_200(self):
+        url = reverse('snack_list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        print('test snack_list status 200: pass')
+
+    def test_list_page_template(self):
+        url = reverse('snack_list')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'snack_list.html')
+        self.assertTemplateUsed(response, 'base.html')
+        print('test snack_templates: pass')
