5ce933ef047f75f4f55bb65a915094d9eadaf265
Monica <ramirez.92.monica@gmail.com>
2023-01-24 19:18:40 -0600

    views, urls, templates add

--- snacks/urls.py
+++ snacks/urls.py
@@ -3,6 +3,6 @@ from .views import SnackDetail, SnackListView
 
 urlpatterns = [
     # snack list is homepage
-    path('', SnackListView.as_view(), name='home'),
+    path('', SnackListView.as_view(), name='snack_list'),
     path('snack/<int:pk>/', SnackDetail.as_view(), name='snack_detail')
 ]
